de25f23611a1603313e31c55804ae5500dee0423
Izaak Dale <izaakdaledev@gmail.com>
2023-06-04 15:59:37 +0100

    Initial commit of simple gin server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello, world",
+		})
+	})
+	router.Run()
 }
